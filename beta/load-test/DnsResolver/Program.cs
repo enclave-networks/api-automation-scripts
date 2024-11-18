955239830bbee789436d6bc6d3ca4bf23671b0d4
Marc Barry <67782240+enclave-marc-barry@users.noreply.github.com>
2024-11-18 17:28:39 +0000

    Update Program.cs

--- beta/load-test/DnsResolver/Program.cs
+++ beta/load-test/DnsResolver/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System.Diagnostics;
 using System.Net;
-using System.Text;
 
 class DnsResolverProgram
 {
@@ -13,10 +12,24 @@ class DnsResolverProgram
     private static readonly ConsoleColor _originalColor = Console.ForegroundColor;
     private static readonly CancellationTokenSource cts = new();
 
-    private static int queryCounter = 0;
+    private static Timer? _dnsReportTimer;
     private static List<string> allHostnames = [];
-    
-    static async Task Main(string[] args)
+
+    private static long _dnsQueriesScheduled;   // waiting for threadpool
+    private static long _dnsQueriesExecuted;    // running in threadpool
+    private static long _dnsQueriesInFlight;
+
+    private static long _querySuccessCount;
+    private static long _queryTimeoutCount;
+    private static long _queryNoAnswerCount;
+    private static long _queryExceptionCount;
+
+    private static long _queryMaxDuration;
+    private static long _queryMinDuration;
+    private static long _querySumOfDurations;
+    private static long _queryCountOfDurations;
+
+    private static async Task Main(string[] args)
     {
         var (concurrency, interval, timeout) = ParseArguments(args);
 
@@ -28,9 +41,15 @@ class DnsResolverProgram
             cts.Cancel();     // trigger cancellation
         };
 
+        ThreadPool.GetMaxThreads(out int workerThreads, out int completionPortThreads);
+        ThreadPool.SetMaxThreads(workerThreads, completionPortThreads);
+
         var tcs = new TaskCompletionSource<bool>();
 
         cts.Token.Register(() => tcs.TrySetResult(true));
+
+        // setup reporting timer
+        _dnsReportTimer = new Timer(DnsQueryVolumeReport, null, 0, 1000);
 
         try
         {
@@ -42,16 +61,15 @@ class DnsResolverProgram
                 // --concurrency=n
                 for (int i = 0; i < concurrency; i++)
                 {
+                    Interlocked.Increment(ref _dnsQueriesScheduled);
+
                     var hostname = hostnames[i];
 
                     _ = Task.Run(async () =>
                     {
                         await ResolveHostnameAsync(hostname, timeout, cts.Token);
-                    });
+                    }, cts.Token);
                 }
-
-                Console.WriteLine($"{concurrency} queries queued.");
-
             }, null, 0, interval);
 
             await tcs.Task;
@@ -114,7 +132,7 @@ class DnsResolverProgram
         return (queryConcurrency, queryInterval, queryTimeout);
     }
 
-    static bool LoadHostnames()
+    private static bool LoadHostnames()
     {
         if (!File.Exists(hostnamesFile))
         {
@@ -138,7 +156,7 @@ class DnsResolverProgram
         return allHostnames.Count > 0;
     }
 
-    static List<string> TakeSlice(int sliceSize)
+    private static List<string> TakeSlice(int sliceSize)
     {
         if (allHostnames.Count < sliceSize)
         {
@@ -155,56 +173,116 @@ class DnsResolverProgram
         return hostnamesToResolve;
     }
 
-    static async Task ResolveHostnameAsync(string hostname, int timeout, CancellationToken cancellationToken)
+    private static async Task ResolveHostnameAsync(string hostname, int timeout, CancellationToken cancellationToken)
     {
-        var success = false;
-        
-        var output = new StringBuilder();
-        
         var queryDuration = Stopwatch.StartNew();
         
-        var counter = Interlocked.Increment(ref queryCounter);
-
-        // --timeout=n
-        var timeoutTask = Task.Delay(TimeSpan.FromMilliseconds(timeout), cancellationToken);
-        
-        var resolverTask = Dns.GetHostAddressesAsync(hostname, cancellationToken);
-
         try
         {
-            
+            Interlocked.Increment(ref _dnsQueriesExecuted);
+            Interlocked.Increment(ref _dnsQueriesInFlight);
+
+            // --timeout=n
+            var timeoutTask = Task.Delay(TimeSpan.FromMilliseconds(timeout), cancellationToken);
+
+            var resolverTask = Dns.GetHostAddressesAsync(hostname, cancellationToken);
+
             var completedTask = await Task.WhenAny(resolverTask, timeoutTask);
 
             if (completedTask == resolverTask)
             {
+                await resolverTask;
+
                 var addresses = await resolverTask;
                 var ipAddresses = addresses.Select(addr => addr.ToString());
 
-                success = true;
-
-                output.Append($"Elapsed: {stopwatch.Elapsed.TotalSeconds,12}s, QueryTime: {queryDuration.Elapsed.TotalMilliseconds,12}ms, Counter: #{counter,-6} {hostname,-64}: {string.Join(", ", ipAddresses)}");
+                Interlocked.Increment(ref _querySuccessCount);
             }
             else
             {
-                output.Append($"Elapsed: {stopwatch.Elapsed.TotalSeconds,12}s, QueryTime: {queryDuration.Elapsed.TotalMilliseconds,12}ms, Counter: #{counter,-6} {hostname,-64}: Query timeout");
+                Interlocked.Increment(ref _queryNoAnswerCount);
             }
         }
         catch (OperationCanceledException)
         {
             // expected, swallow
         }
-        catch (Exception ex)
+        catch (Exception)
         {
-            output.Append($"Elapsed: {stopwatch.Elapsed.TotalSeconds,12}s, QueryTime: {queryDuration.Elapsed.TotalMilliseconds,12}ms, Counter: #{counter,-6} {hostname,-64}: {ex.Message}");
+            Interlocked.Increment(ref _queryExceptionCount);
         }
         finally
         {
-            lock (_consoleLock)
+            Interlocked.Decrement(ref _dnsQueriesInFlight);
+
+            var duration = (uint)stopwatch.ElapsedMilliseconds;
+            var currentMax = Interlocked.Read(ref _queryMaxDuration);
+            var currentMin = Interlocked.Read(ref _queryMinDuration);
+
+            Interlocked.Increment(ref _queryCountOfDurations);
+
+            Interlocked.Add(ref _querySumOfDurations, duration);
+
+            if (duration > currentMax)
             {
-                Console.ForegroundColor = (success == true) ? ConsoleColor.DarkGreen : ConsoleColor.DarkRed;
-                Console.WriteLine(output);
-                Console.ForegroundColor = _originalColor;
+                Interlocked.Exchange(ref _queryMaxDuration, duration);
             }
+
+            if (duration < currentMin)
+            {
+                Interlocked.Exchange(ref _queryMinDuration, duration);
+            }
+        }
+    }
+
+    private static void DnsQueryVolumeReport(object? state)
+    {
+        var queriesScheduled = Interlocked.Read(ref _dnsQueriesScheduled);
+        var queriesExecuted = Interlocked.Read(ref _dnsQueriesExecuted);
+        var queriesInFlight = Interlocked.Read(ref _dnsQueriesInFlight);
+
+        Interlocked.Exchange(ref _dnsQueriesExecuted, 0); // reset the count after every second
+
+        var querySuccessCount = Interlocked.Read(ref _querySuccessCount);
+        var queryTimeoutCount = Interlocked.Read(ref _queryTimeoutCount);
+        var queryNoAnswerCount = Interlocked.Read(ref _queryNoAnswerCount);
+        var queryExceptionCount = Interlocked.Read(ref _queryExceptionCount);
+
+        Interlocked.Exchange(ref _querySuccessCount, 0);
+        Interlocked.Exchange(ref _queryTimeoutCount, 0);
+        Interlocked.Exchange(ref _queryNoAnswerCount, 0);
+        Interlocked.Exchange(ref _queryExceptionCount, 0);
+
+        var queryMinDuration = Interlocked.Read(ref _queryMinDuration);
+        var queryMaxDuration = Interlocked.Read(ref _queryMaxDuration);
+        var querySumOfDurations = Interlocked.Read(ref _querySumOfDurations);
+        var queryCountOfDurations = Interlocked.Read(ref _queryCountOfDurations);
+
+        Interlocked.Exchange(ref _queryMinDuration, long.MaxValue);
+        Interlocked.Exchange(ref _queryMaxDuration, 0);
+        Interlocked.Exchange(ref _querySumOfDurations, 0);
+        Interlocked.Exchange(ref _queryCountOfDurations, 0);
+
+        var failed = queryNoAnswerCount + queryTimeoutCount + queryExceptionCount;
+        var total = querySuccessCount + failed;
+        var successPercentage = total > 0 ? (double)querySuccessCount / total * 100 : 0;
+
+        var averageDuration = queriesScheduled > 0 ? (double)querySumOfDurations / queriesScheduled : 0;
+
+        // clamp min duration to 0 if all in-flights queries are yet to complete (so no minimum duration is known)
+        queryMinDuration = queryMinDuration == long.MaxValue ? 0 : queryMinDuration;
+
+        if (total > 0)
+        {
+            Console.WriteLine(
+                "DNS queries queued: {0,-6} Queries scheduled per second: {1,-6} In-flight: {2,-6} Avg duration: {3,-4} ms, Success: {4,-4} ({5,3}%) Failed: {6,-4}",
+                queriesScheduled,
+                queriesExecuted,
+                queriesInFlight,
+                Math.Round(averageDuration, 0),
+                querySuccessCount,
+                Math.Round(successPercentage, 0),
+                failed);
         }
     }
 }
